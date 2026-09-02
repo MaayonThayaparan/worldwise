973c6766a8f5cd33fb1b81422c67d73f06d273d8
Maayon Thayaparan <maayon.thayaparan@gmail.com>
2026-09-02 01:04:31 -0400

    Adding navigation

---
+++ src/components/Map.jsx
@@ -0,0 +1,26 @@
+import { useSearchParams, useNavigate } from "react-router-dom";
+import styles from "./Map.module.css";
+
+function Map() {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParamns] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>
+      <h1>
+        Position {lat}, {lng}
+      </h1>
+      <button
+        onClick={() => {
+          setSearchParamns({ lat: 23, lng: 25 });
+        }}
+      >
+        Change pos
+      </button>
+    </div>
+  );
+}
+
+export default Map;
